e018afc04fbcf633e4c36e3374bd8f0579c3957a
Cornucopib <13040839537@163.com>
2023-05-07 22:18:02 +0800

    利用maven+profiles+maven-assembly-plugin实现指定文件，打jar包。

---
+++ src/main/java/com/cornucopib/specificfiles/dir1/A.java
@@ -0,0 +1,15 @@
+package com.cornucopib.specificfiles.dir1;
+
+/**
+ * A类
+ *
+ * @author cornucopib
+ * @since 2023/5/7
+ */
+public class A {
+
+    public static void main(String[] args) {
+        System.out.println("A");
+    }
+
+}
---
+++ src/main/java/com/cornucopib/specificfiles/dir2/B.java
@@ -0,0 +1,13 @@
+package com.cornucopib.specificfiles.dir2;
+
+/**
+ * @author cornucopib
+ * @since 2023/5/7
+ */
+public class B {
+
+    public static void main(String[] args) {
+        System.out.println("B");
+    }
+
+}
